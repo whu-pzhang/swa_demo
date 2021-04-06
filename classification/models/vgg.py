836a157aa0be489582d8327e886cb61c7269943d
whu-pzhang <pzhang@whu.edu.cn>
2021-04-06 21:17:01 +0800

    add modified vgg model

---
+++ classification/models/vgg.py
@@ -0,0 +1,83 @@
+"""
+    VGG model definition
+    ported from https://github.com/pytorch/vision/blob/master/torchvision/models/vgg.py
+"""
+
+import torch.nn as nn
+from torchvision.models.utils import load_state_dict_from_url
+
+__all__ = ['vgg16', 'vgg16_bn']
+
+cfgs = {
+    16: [
+        64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M',
+        512, 512, 512, 'M'
+    ],
+    19: [
+        64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512, 512, 512,
+        512, 'M', 512, 512, 512, 512, 'M'
+    ],
+}
+
+
+class VGG(nn.Module):
+    def __init__(self, features, num_classes=10, init_weights=True):
+        super(VGG, self).__init__()
+        self.features = features
+        self.classifier = nn.Sequential(
+            nn.Dropout(),
+            nn.Linear(512, 512),
+            nn.ReLU(True),
+            nn.Dropout(),
+            nn.Linear(512, 512),
+            nn.ReLU(True),
+            nn.Linear(512, num_classes),
+        )
+
+        if init_weights:
+            self.init_weights()
+
+    def init_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight,
+                                        mode='fan_out',
+                                        nonlinearity='relu')
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                nn.init.normal_(m.weight, 0, 0.01)
+                nn.init.constant_(m.bias, 0)
+
+    def forward(self, x):
+        x = self.features(x)
+        x = x.view(x.size(0), -1)
+        x = self.classifier(x)
+        return x
+
+
+def make_layers(cfg, batch_norm=False):
+    layers = list()
+    in_channels = 3
+    for v in cfg:
+        if v == 'M':
+            layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+        else:
+            conv2d = nn.Conv2d(in_channels, v, kernel_size=3, padding=1)
+            if batch_norm:
+                layers += [conv2d, nn.BatchNorm2d(v), nn.ReLU(inplace=True)]
+            else:
+                layers += [conv2d, nn.ReLU(inplace=True)]
+            in_channels = v
+    return nn.Sequential(*layers)
+
+
+def vgg16(**kwargs):
+    return VGG(make_layers(cfgs[16], batch_norm=False), **kwargs)
+
+
+def vgg16_bn(**kwargs):
+    return VGG(make_layers(cfgs[16], batch_norm=True), **kwargs)
